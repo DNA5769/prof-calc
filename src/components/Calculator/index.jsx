9d2e4b24fd4f394f362b581da208f78cb565b027
DNA5769 <dennisthomas2002@gmail.com>
2021-09-08 11:17:57 +0530

    Created calculator structure

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import './styles.css';
 
-const Calculator = () => {   
+const Calculator = () => {
+    const calcList = [
+        ['7', '8', '9', 'X'],
+        ['4', '5', '6', '-'],
+        ['1', '2', '3', '+'],
+        ['0', '', '=', '/'],
+    ];
+
     return (
-        <div className="calc">
-            Calculator
+        <div className="calc container">
+            <div className="row">
+                <div className="col-12">
+                    9+10
+                </div>
+            </div>
+            {calcList.map((row, i) => (
+                <div key={i} className="row">
+                    {row.map((el, j) => (
+                        <div key={j} className="col-3">
+                            {el}
+                        </div>
+                    ))}
+                </div>
+            ))}
+            <button>Clear</button>
         </div>
     );
 }
