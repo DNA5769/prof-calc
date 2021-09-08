d49f1e868abf1eb73d4c6a98992597502b01f7ab
DNA5769 <dennisthomas2002@gmail.com>
2021-09-08 11:31:05 +0530

    Finish calculator logic

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,31 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Parser } from 'expr-eval';
 import './styles.css';
 
 const Calculator = () => {
+    const parser = new Parser();
     const calcList = [
-        ['7', '8', '9', 'X'],
+        ['7', '8', '9', '*'],
         ['4', '5', '6', '-'],
         ['1', '2', '3', '+'],
         ['0', '', '=', '/'],
     ];
 
+    const [exp, setExp] = useState('');
+    const [inpFl, setInpFl] = useState(true);
+
+    const handleInput = (el) => {
+        if (el === '=')
+        {
+            setExp(parser.parse(exp).evaluate());
+            setInpFl(true);
+        }
+        else
+        {
+            if (!inpFl)
+                setExp(exp+el);
+            else
+            {
+                setExp(el);
+                setInpFl(false);
+            }
+        }
+    }
+
     return (
         <div className="calc container">
             <div className="row">
                 <div className="col-12">
-                    9+10
+                    {exp}
                 </div>
             </div>
             {calcList.map((row, i) => (
                 <div key={i} className="row">
                     {row.map((el, j) => (
-                        <div key={j} className="col-3">
+                        <div key={j} className="col-3" onClick={() => handleInput(el)}>
                             {el}
                         </div>
                     ))}
                 </div>
             ))}
-            <button>Clear</button>
+            <div className="row">
+                <div className="col-12" onClick={() => setExp('')}>
+                    Clear
+                </div>
+            </div>
         </div>
     );
 }
