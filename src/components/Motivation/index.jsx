9cba6021a834161dfa57efed5eaf1025ae01372f
DNA5769 <dennisthomas2002@gmail.com>
2021-09-08 10:58:20 +0530

    Setup motivational quote fetching

--- src/components/Motivation/index.jsx
+++ src/components/Motivation/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles.css';
 
 const Motivation = () => {
+    const [quote, setQuote] = useState('');
+
+    useEffect(() => {
+        fetch('https://favqs.com/api/qotd')
+            .then(res => res.json())
+            .then(data => setQuote(data.quote.body));
+    }, []);
+    
     return (
         <div className="motiv">
-            Motivation
+            <p>{quote}</p>
         </div>
     )
 }
