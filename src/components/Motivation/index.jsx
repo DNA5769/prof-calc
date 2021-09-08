131f4882358f4eec735c612bfa38ab0adadc5b23
DNA5769 <dennisthomas2002@gmail.com>
2021-09-08 12:22:38 +0530

    Added documentation to motivation

--- src/components/Motivation/index.jsx
+++ src/components/Motivation/index.jsx
@@ -4,12 +4,14 @@ import './styles.css';
 const Motivation = () => {
     const [quote, setQuote] = useState('');
 
+    // Function which fetches quote from the API and sets it as quote
     const getQuote = () => {
         fetch('https://favqs.com/api/qotd')
             .then(res => res.json())
             .then(data => setQuote(data.quote.body));
     };
 
+    // Getting a quote when the component gets mounted initially
     useEffect(() => {
         getQuote();
     }, []);
@@ -18,6 +20,7 @@ const Motivation = () => {
         <div className="motiv p-3">
             <p>{quote}</p>
             <div className="d-flex flex-row-reverse">
+                {/* Getting new quote whenever user clicks on the Refresh button */}
                 <button type="button" class="btn btn-outline-dark" onClick={getQuote}>Refresh</button>
             </div>
         </div>
