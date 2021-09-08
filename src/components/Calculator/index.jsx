d6ec544f0488d8c8590d8c20112e89f5ad3ef74e
DNA5769 <dennisthomas2002@gmail.com>
2021-09-08 12:15:56 +0530

    Added documentation to calculator

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -4,6 +4,9 @@ import './styles.css';
 
 const Calculator = () => {
     const parser = new Parser();
+
+    // Using an array of arrays to represent the calculator which we will use
+    // to render the calculator
     const calcList = [
         ['7', '8', '9', '*'],
         ['4', '5', '6', '-'],
@@ -11,13 +14,26 @@ const Calculator = () => {
         ['0', '', '=', '/'],
     ];
 
+    // The main expression which we see in the top of the calculator
     const [exp, setExp] = useState('Type expression!');
+
+    // This is a flag which is used to indicate whether the input should clear
+    // the expression or should it append to the expression. The necessity for this flag
+    // is that after we calculate the result of the expression, any input after should clear
+    // the expression, whereas in other cases, it should append to it
     const [inpFl, setInpFl] = useState(true);
 
     const handleInput = (el) => {
+        // If '=' is pressed we need to calculate the result
         if (el === '=')
         {
+            // Wanted to use anything other than eval() to calculate the result because
+            // the only thing getting injected should be us and not our websites
+            // #getvaccinated
             setExp(parser.parse(exp).evaluate());
+
+            // Since we calculated the result, we need to clear the expression on the next input
+            // so we set the flag to true
             setInpFl(true);
         }
         else
@@ -39,6 +55,7 @@ const Calculator = () => {
                     {exp}
                 </div>
             </div>
+            {/* Rendering calculator from the array of arrays we made */}
             {calcList.map((row, i) => (
                 <div key={i} className="row">
                     {row.map((el, j) => (
